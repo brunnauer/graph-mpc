3973476dfaf719108a751ba5e237996b72e5f7ee
brunnauer <simon.brunnauer@proton.me>
2025-05-03 14:28:56 +0200

    Fix typo in ubuntu script

--- scripts/ubuntu/test_shuffle.sh
+++ scripts/ubuntu/test_shuffle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tmux new-session -d -s session1 'cd ./build/tests/test_shuffle && ./test_shuffle -p 0 --localhost'
-tmux new-session -d -s session1 'cd ./build/tests/test_shuffle && ./test_shuffle -p 1 --localhost'
-tmux new-session -d -s session1 'cd ./build/tests/test_shuffle && ./test_shuffle -p 2 --localhost'
+tmux new-session -d -s session2 'cd ./build/tests/test_shuffle && ./test_shuffle -p 1 --localhost'
+tmux new-session -d -s session3 'cd ./build/tests/test_shuffle && ./test_shuffle -p 2 --localhost'
