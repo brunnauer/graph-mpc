b187a828dd0079e5442de2299e8788a2f906e1c4
brunnauer <simon.brunnauer@proton.me>
2025-05-21 09:21:51 +0200

    Add shuffle_num option to scripts

--- scripts/macos/test_shuffle_dealer_p0.sh
+++ scripts/macos/test_shuffle_dealer_p0.sh
@@ -6,12 +6,12 @@ tell application "Terminal"
     activate
 
     -- Window 1
-    set w1 to do script "cd ~/CODE/GraphMPC/build/tests/test_shuffle && ./test_shuffle -p 0 --localhost"
+    set w1 to do script "cd ~/CODE/GraphMPC/build/tests/test_shuffle && ./test_shuffle -p 0 --shuffle_num 2 --localhost"
     delay 0.2
     set bounds of front window to {0, 0, 500, 800} -- {left, top, right, bottom}
 
     -- Window 2
-    set w2 to do script "cd ~/CODE/GraphMPC/build/tests/test_shuffle && ./test_shuffle -p 2 --localhost"
+    set w2 to do script "cd ~/CODE/GraphMPC/build/tests/test_shuffle && ./test_shuffle -p 2 --shuffle_num 2 --localhost"
     delay 0.2
     set bounds of front window to {500, 0, 1000, 800}
 end tell
