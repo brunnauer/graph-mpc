56412bc9db30563c4177904eb4616cb2a0c6895b
brunnauer <simon.brunnauer@proton.me>
2025-06-18 11:32:39 +0200

    Add communication assertions to test_eqz

--- tests/test_eqz.cpp
+++ tests/test_eqz.cpp
@@ -4,61 +4,54 @@
 #include "../src/protocol/clip.h"
 #include "../src/utils/random_generators.h"
 #include "../src/utils/sharing.h"
+#include "constants.h"
 
 void test_eqz(Party id, RandomGenerators &rngs, std::shared_ptr<io::NetIOMP> network, size_t n, size_t BLOCK_SIZE) {
     json output_data;
-
-    std::vector<Ring> x_vec(n);
-    std::vector<Ring> y_vec(n);
-
-    std::vector<Ring> share_x_vec(n);
-    std::vector<Ring> share_y_vec(n);
-
-    for (size_t i = 0; i < n; ++i) {
-        x_vec[i] = std::rand() % 2;
-    }
-    for (size_t i = 0; i < n / 2; ++i) {
-        y_vec[i] = 1;
-    }
-    for (size_t i = n / 2; i < n; ++i) {
-        y_vec[i] = 0;
-    }
-
-    share_x_vec = share::random_share_secret_vec_2P(id, rngs, x_vec);
-    share_y_vec = share::random_share_secret_vec_2P(id, rngs, y_vec);
-
-    auto triples = mul::preprocess(id, rngs, network, n, BLOCK_SIZE);
-    auto x_y_share = mul::evaluate(id, network, n, BLOCK_SIZE, triples, share_x_vec, share_y_vec);
-    auto x_y_revealed = share::reveal_vec(id, network, BLOCK_SIZE, x_y_share);
-
-    if (id != D) {
-        std::cout << "x: ";
-        for (auto &elem : x_vec) {
-            std::cout << elem << ", ";
-        }
-        std::cout << std::endl;
-
-        std::cout << "y: ";
-        for (auto &elem : y_vec) {
-            std::cout << elem << ", ";
-        }
-        std::cout << std::endl;
-
-        std::cout << "Result of multiplication: ";
-        for (auto &elem : x_y_revealed) {
-            std::cout << elem << ", ";
-        }
-        std::cout << std::endl;
-    }
 
     std::cout << std::endl << "------ test_eqz ------" << std::endl;
     std::vector<Ring> test_vec(n);
     for (size_t i = 0; i < n; ++i) test_vec[i] = i;
     auto test_vec_share = share::random_share_secret_vec_2P(id, rngs, test_vec);
 
-    auto eqz_vec = clip::equals_zero(id, rngs, network, BLOCK_SIZE, test_vec_share);
-    std::vector<Ring> B2A_vec = clip::B2A(id, rngs, network, BLOCK_SIZE, eqz_vec);
+    StatsPoint start_pre(*network);
+    auto eqz_triples = clip::equals_zero_preprocess(id, rngs, network, n, BLOCK_SIZE);
+    StatsPoint end_pre(*network);
 
+    auto rbench_pre = end_pre - start_pre;
+    output_data["benchmarks_pre"].push_back(rbench_pre);
+    size_t bytes_sent_pre = 0;
+    for (const auto &val : rbench_pre["communication"]) {
+        bytes_sent_pre += val.get<int64_t>();
+    }
+
+    /* Preprocessing communication assertions */
+    if (id == D) {
+        /* n_elems * 4 Bytes per element */
+        size_t total_comm = 4 * eqz_comm_pre(n);
+        assert(bytes_sent_pre == total_comm);
+    }
+
+    StatsPoint start_online(*network);
+    auto eqz_vec = clip::equals_zero_evaluate(id, rngs, network, BLOCK_SIZE, eqz_triples, test_vec_share);
+    StatsPoint end_online(*network);
+
+    auto rbench = end_online - start_online;
+    output_data["benchmarks"].push_back(rbench);
+
+    size_t bytes_sent = 0;
+    for (const auto &val : rbench["communication"]) {
+        bytes_sent += val.get<int64_t>();
+    }
+
+    /* Evaluation communication assertions */
+    if (id != D) {
+        size_t total_comm = 4 * eqz_comm_online(n);
+        assert(total_comm == bytes_sent);
+    }
+
+    /* Correctness assertions */
+    std::vector<Ring> B2A_vec = clip::B2A(id, rngs, network, BLOCK_SIZE, eqz_vec);
     auto result = share::reveal_vec(id, network, BLOCK_SIZE, B2A_vec);
 
     if (id != D) {
